4d9a2f844460ad759d1ecb5da9ddb0e77e788d72
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-08-15 09:20:26 -0400

    Make the package test actually test the package

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -2,6 +2,14 @@
 #include <ga3/ga3.hpp>
 
 int main() {
-    hello();
+    ga3::population pop{
+            1,
+            {{0,1}},
+            [](auto genes) -> auto
+            {
+                return 1.0;
+            }
+    };
+    std::cout << "GA3 package testing: OK!" << std::endl;
 }
 
